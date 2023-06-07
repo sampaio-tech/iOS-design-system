7b6cc5be351ae0048fa670c9359149a4b4ba2851
pedrohsampaioo <pedrohsampaioo@outlook.com>
2023-06-07 10:26:59 -0300

    feat: create `DescriptionWidget`

---
+++ lib/src/widgets/description_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import '../../ios_design_system.dart';
+
+class DescriptionWidget extends StatelessWidget {
+  final String text;
+  final EdgeInsetsGeometry? padding;
+  const DescriptionWidget({
+    super.key,
+    required this.text,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = CupertinoTheme.brightnessOf(context);
+    return Padding(
+      padding: padding ??
+          const EdgeInsets.symmetric(horizontal: 16).copyWith(top: 8),
+      child: Text(
+        text,
+        style: AppTypography.caption1Regular.copyWith(
+            color: switch (brightness) {
+          Brightness.light => DefaultLabelColors.secondaryLight,
+          Brightness.dark => DefaultLabelColors.secondaryDark,
+        }),
+        textAlign: TextAlign.start,
+        overflow: TextOverflow.visible,
+      ),
+    );
+  }
+}
