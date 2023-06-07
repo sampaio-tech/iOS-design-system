cfee6a335390bbd6bb9bccfa969ae479aa2e479e
pedrohsampaioo <pedrohsampaioo@outlook.com>
2023-06-07 10:25:37 -0300

    feat: create `DividerWidget`

---
+++ lib/src/widgets/divider_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import '../../ios_design_system.dart';
+
+class DividerWidget extends StatelessWidget {
+  const DividerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = CupertinoTheme.brightnessOf(context);
+    return AnimatedContainer(
+      duration: kAnimationInDuration,
+      height: 1,
+      color: switch (brightness) {
+        Brightness.light => SystemColoursSeparatorColors.nonOpaqueLight,
+        Brightness.dark => SystemColoursSeparatorColors.nonOpaqueDark,
+      },
+    );
+  }
+}
