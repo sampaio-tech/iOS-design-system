cd6be9eb05524210a83e9473cc93457feeb61123
pedrohsampaioo <pedrohsampaioo@outlook.com>
2023-06-25 16:26:24 -0300

    feat: create `ModalSheetWidget`

--- lib/src/theme/settings/constants.dart
+++ lib/src/theme/settings/constants.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 const kCupertinoButtonPressedOpacity = 0.8;
@@ -5,3 +7,5 @@ const kCupertinoButtonBorderRadius = BorderRadius.all(Radius.circular(6));
 const kCupertinoButtonPadding = EdgeInsets.all(0);
 const kAnimationOutDuration = Duration(milliseconds: 120);
 const kAnimationInDuration = Duration(milliseconds: 180);
+const kMinimumSafeAreaPadding = EdgeInsets.only(bottom: 34);
+final kImageFilterBlur = ImageFilter.blur(sigmaY: 10, sigmaX: 10);
