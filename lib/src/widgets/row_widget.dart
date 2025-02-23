a7d9655e7b9885a0779e4e922de13730b8aef647
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-02-22 22:00:34 -0300

    feat: allow widget types in `RowWidget`

--- lib/src/widgets/row_widget.dart
+++ lib/src/widgets/row_widget.dart
@@ -8,12 +8,48 @@ class RowWidget extends StatelessWidget {
     required this.description,
     required this.displayDivider,
     required this.onPressed,
+    required this.leftWidget,
+    required this.rightWidget,
     super.key,
-    this.leftWidget,
-    this.rightWidget,
   });
-  final String title;
-  final String? description;
+
+  factory RowWidget.standard({
+    required String title,
+    required String? description,
+    required Widget? leftWidget,
+    required Widget? rightWidget,
+    required bool displayDivider,
+    required VoidCallback? onPressed,
+  }) => RowWidget(
+    title:
+        (theme) => Text(
+          title,
+          style: theme.typography.bodyRegular.copyWith(
+            color: theme.defaultLabelColors.primary,
+          ),
+          textAlign: TextAlign.start,
+          overflow: TextOverflow.visible,
+        ),
+    description: switch (description) {
+      null => null,
+      final description =>
+        (theme) => Text(
+          description,
+          style: theme.typography.caption1Regular.copyWith(
+            color: theme.defaultLabelColors.secondary,
+          ),
+          textAlign: TextAlign.start,
+          overflow: TextOverflow.visible,
+        ),
+    },
+    displayDivider: displayDivider,
+    onPressed: onPressed,
+    leftWidget: leftWidget,
+    rightWidget: rightWidget,
+  );
+
+  final Widget Function(IosThemeData theme) title;
+  final Widget Function(IosThemeData theme)? description;
   final Widget? leftWidget;
   final Widget? rightWidget;
   final bool displayDivider;
@@ -46,8 +82,9 @@ class RowWidget extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12)
-                        .copyWith(right: 16),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                    ).copyWith(right: 16),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -57,26 +94,12 @@ class RowWidget extends StatelessWidget {
                             crossAxisAlignment: CrossAxisAlignment.stretch,
                             mainAxisSize: MainAxisSize.min,
                             children: [
-                              Text(
-                                title,
-                                style: theme.typography.bodyRegular.copyWith(
-                                  color: theme.defaultLabelColors.primary,
-                                ),
-                                textAlign: TextAlign.start,
-                                overflow: TextOverflow.visible,
-                              ),
+                              title(theme),
                               if (description != null)
                                 const SizedBox(height: 2),
                               if (description != null)
-                                Text(
-                                  description!,
-                                  style:
-                                      theme.typography.caption1Regular.copyWith(
-                                    color: theme.defaultLabelColors.secondary,
-                                  ),
-                                  textAlign: TextAlign.start,
-                                  overflow: TextOverflow.visible,
-                                ),
+                                description?.call(theme) ??
+                                    const SizedBox.shrink(),
                             ],
                           ),
                         ),
