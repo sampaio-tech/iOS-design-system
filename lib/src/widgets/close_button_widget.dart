3f3911158d74ae1f1651ea7da905e772127eaec7
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-03-02 22:26:29 -0300

    feat: use `closeRounded` svg in `CloseButtonWidget`

--- lib/src/widgets/close_button_widget.dart
+++ lib/src/widgets/close_button_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../ios_design_system.dart';
+import '../assets_gen/assets.gen.dart';
 
 class CloseButtonWidget extends StatelessWidget {
   const CloseButtonWidget({super.key, this.onPressed});
@@ -32,18 +33,17 @@ class CloseButtonWidget extends StatelessWidget {
               ),
             ),
             Center(
-              child: IconWidget.transparentBackground(
-                iconData: Icons.close_rounded,
-                iconSize: 21,
-                iconColorCallback:
-                    (theme) => switch (theme) {
-                      IosLightThemeData() => const Color(
-                        0xFF3C3C43,
-                      ).withValues(alpha: .6),
-                      IosDarkThemeData() => const Color(
-                        0xFFEBEBF5,
-                      ).withValues(alpha: .6),
-                    },
+              child: Assets.lib.src.assets.svg.closeRounded.svg(
+                height: 16,
+                width: 16,
+                colorFilter: ColorFilter.mode(switch (theme) {
+                  IosLightThemeData() => const Color(
+                    0xFF3C3C43,
+                  ).withValues(alpha: .6),
+                  IosDarkThemeData() => const Color(
+                    0xFFEBEBF5,
+                  ).withValues(alpha: .6),
+                }, BlendMode.srcIn),
               ),
             ),
           ],
