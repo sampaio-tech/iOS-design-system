1b0b4236d5706119d9c7c944466e6e185a7e5e30
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-03-02 22:26:11 -0300

    chore: run `build_runner`

---
+++ lib/src/assets_gen/assets.gen.dart
@@ -0,0 +1,128 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $LibGen {
+  const $LibGen();
+
+  /// Directory path: lib/src
+  $LibSrcGen get src => const $LibSrcGen();
+}
+
+class $LibSrcGen {
+  const $LibSrcGen();
+
+  /// Directory path: lib/src/assets
+  $LibSrcAssetsGen get assets => const $LibSrcAssetsGen();
+}
+
+class $LibSrcAssetsGen {
+  const $LibSrcAssetsGen();
+
+  /// Directory path: lib/src/assets/svg
+  $LibSrcAssetsSvgGen get svg => const $LibSrcAssetsSvgGen();
+}
+
+class $LibSrcAssetsSvgGen {
+  const $LibSrcAssetsSvgGen();
+
+  /// File path: lib/src/assets/svg/close_rounded.svg
+  SvgGenImage get closeRounded =>
+      const SvgGenImage('lib/src/assets/svg/close_rounded.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [closeRounded];
+}
+
+class Assets {
+  const Assets._();
+
+  static const String package = 'ios_design_system';
+
+  static const $LibGen lib = $LibGen();
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  static const String package = 'ios_design_system';
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/ios_design_system/$_assetName';
+}
