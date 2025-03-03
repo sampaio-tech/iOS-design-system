cc465fd1282d8506ae4c5c4ce8a335f8ae4c3248
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-03-03 17:52:53 -0300

    feat: create `apple play` variation of `DividerWidget`

--- lib/src/widgets/divider_widget.dart
+++ lib/src/widgets/divider_widget.dart
@@ -2,15 +2,29 @@ import 'package:flutter/cupertino.dart';
 
 import '../../ios_design_system.dart';
 
+Color applePlayDividerColorCallback01(IosThemeData theme) => switch (theme) {
+  IosLightThemeData() => const Color(0xFFC6C6C8),
+  IosDarkThemeData() => const Color(0xFF545458).withValues(alpha: .65),
+};
+
 class DividerWidget extends StatelessWidget {
-  const DividerWidget({super.key});
+  const DividerWidget({super.key, this.height, this.colorCallback});
+
+  static const applePay = const DividerWidget(
+    colorCallback: applePlayDividerColorCallback01,
+  );
+
+  final double? height;
+  final Color? Function(IosThemeData theme)? colorCallback;
 
   @override
   Widget build(BuildContext context) {
     final theme = IosTheme.of(context);
     return Container(
-      height: 1,
-      color: theme.systemColoursSeparatorColors.nonOpaque,
+      height: height ?? 1,
+      color:
+          colorCallback?.call(theme) ??
+          theme.systemColoursSeparatorColors.nonOpaque,
     );
   }
 }
