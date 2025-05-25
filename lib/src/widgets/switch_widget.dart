aeebd63cf6dc1eedd845a947ea4667cebeb59826
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-05-24 21:59:53 -0300

    feat: create `SwitchWidget.stocks`

--- lib/src/widgets/switch_widget.dart
+++ lib/src/widgets/switch_widget.dart
@@ -2,14 +2,39 @@ import 'package:flutter/cupertino.dart';
 
 import '../../ios_design_system.dart';
 
+Color stocksDecorationCallback(IosThemeData theme) =>
+    theme.stocksDecorations.defaultColors.positiveTrend;
+
 class SwitchWidget extends StatelessWidget {
   const SwitchWidget({
     required this.value,
     super.key,
     this.onChanged,
+    this.activeTrackColorCallback,
+    this.inactiveTrackColorCallback,
+    this.thumbColorCallback,
   });
+
+  factory SwitchWidget.stocks({
+    required bool value,
+    void Function(bool)? onChanged,
+    Color Function(IosThemeData)? activeTrackColorCallback =
+        stocksDecorationCallback,
+    Color Function(IosThemeData)? inactiveTrackColorCallback,
+    Color Function(IosThemeData)? thumbColorCallback,
+  }) => SwitchWidget(
+    value: value,
+    onChanged: onChanged,
+    activeTrackColorCallback: activeTrackColorCallback,
+    inactiveTrackColorCallback: inactiveTrackColorCallback,
+    thumbColorCallback: thumbColorCallback,
+  );
+
   final void Function(bool)? onChanged;
   final bool value;
+  final Color Function(IosThemeData theme)? activeTrackColorCallback;
+  final Color Function(IosThemeData theme)? inactiveTrackColorCallback;
+  final Color Function(IosThemeData theme)? thumbColorCallback;
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +42,14 @@ class SwitchWidget extends StatelessWidget {
     return CupertinoSwitch(
       value: value,
       onChanged: onChanged,
-      activeTrackColor: theme.defaultColors.systemGreen,
-      inactiveTrackColor: theme.defaultFillColors.secondary,
-      thumbColor: theme.defaultColors.systemWhite,
+      activeTrackColor:
+          activeTrackColorCallback?.call(theme) ??
+          theme.defaultColors.systemGreen,
+      inactiveTrackColor:
+          inactiveTrackColorCallback?.call(theme) ??
+          theme.defaultFillColors.secondary,
+      thumbColor:
+          thumbColorCallback?.call(theme) ?? theme.defaultColors.systemWhite,
     );
   }
 }
