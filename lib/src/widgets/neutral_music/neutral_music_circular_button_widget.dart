03ca6a76c0a5f90ba7a6f5a7f4740b14dcf108e0
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-05-26 00:13:53 -0300

    feat: create `NeutralMusicCircularButtonWidget`

---
+++ lib/src/widgets/neutral_music/neutral_music_circular_button_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../../../ios_design_system.dart';
+
+class NeutralMusicCircularButtonWidget extends StatelessWidget {
+  const NeutralMusicCircularButtonWidget({
+    super.key,
+    this.icon,
+    this.onLongPress,
+    this.onPressed,
+    this.borderRadius,
+  });
+
+  factory NeutralMusicCircularButtonWidget.icon({
+    required IconData icon,
+    double iconSize = 16,
+    void Function()? onPressed,
+    void Function()? onLongPress,
+    BorderRadius? borderRadius,
+  }) => NeutralMusicCircularButtonWidget(
+    icon: Builder(
+      builder: (context) {
+        final theme = IosTheme.of(context);
+        return Icon(
+          icon,
+          size: iconSize,
+          color: theme.neutralMusicDecorations.defaultColors.title,
+        );
+      },
+    ),
+    onPressed: onPressed,
+    onLongPress: onLongPress,
+    borderRadius: borderRadius,
+  );
+
+  factory NeutralMusicCircularButtonWidget.text({
+    required String text,
+    TextStyle? textStyle,
+    void Function()? onPressed,
+    void Function()? onLongPress,
+    BorderRadius? borderRadius,
+  }) => NeutralMusicCircularButtonWidget(
+    icon: Builder(
+      builder: (context) {
+        final theme = IosTheme.of(context);
+        return Text(
+          text,
+          style:
+              textStyle ??
+              theme.typography.subheadlineRegular.copyWith(
+                color: theme.neutralMusicDecorations.defaultColors.title,
+              ),
+        );
+      },
+    ),
+    onPressed: onPressed,
+    onLongPress: onLongPress,
+    borderRadius: borderRadius,
+  );
+
+  final Widget? icon;
+  final void Function()? onPressed;
+  final void Function()? onLongPress;
+  final BorderRadius? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = IosTheme.of(context);
+    final button = CupertinoButtonWidget(
+      onPressed: onPressed,
+      onLongPress: onLongPress,
+      child: Container(
+        decoration:
+            theme.neutralMusicDecorations.gradients.backgroundCircularButton,
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            switch (icon) {
+              null => const SizedBox.shrink(),
+              final icon => icon,
+            },
+          ],
+        ),
+      ),
+    );
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        switch (borderRadius) {
+          null => ClipOval(child: button),
+          final borderRadius => ClipRRect(
+            borderRadius: borderRadius,
+            child: button,
+          ),
+        },
+      ],
+    );
+  }
+}
