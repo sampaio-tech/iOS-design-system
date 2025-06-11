aef0d63f90bc14d549a91d37274e6ab36052ec9c
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-06-11 19:29:40 -0300

    fix: fix compiler error in `example`

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const themeData = IosDarkThemeData();
+    final themeData = IosDarkThemeData();
     return IosAnimatedTheme(
       data: themeData,
       child: CupertinoApp(
